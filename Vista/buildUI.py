fcb35eb83fee88e0aa4c82d266ae9a0b376cd39d
BuildTools <unconfigured@null.spigotmc.org>
2021-01-21 20:19:41 +0100

    fixed bugs

--- Vista/buildUI.py
+++ Vista/buildUI.py
@@ -14,51 +14,51 @@ file.close()
 MaxPlayers = 10
 UsersGame = set()
 
+
 class MainWindow:
     def __init__(self, root):
-        self.c = Controller.Controller('RGAPI-580545bf-2972-45f2-a1df-3bf5e8fe6f96', 'euw1')
+        self.c = Controller.Controller('RGAPI-179ffd3e-16c3-49fa-b440-6143b07bfc61', 'euw1')
         self.root = root
         self.root.resizable(width=False, height=False)
 
-        self.content = Frame(self.root, width= 1512, height=1512)
+        self.content = Frame(self.root, width=1512, height=1512)
         self.root.title("League Customs")
         self.content.grid(column=0, row=0)
 
         # Up left
-        repositoriFrame = Frame(self.content, width= 128, height=128)
+        repositoriFrame = Frame(self.content, width=128, height=128)
         repositoriFrame.grid(column=0, row=0, padx=16, pady=16)
         # add user to list
-        addToAll = Button(repositoriFrame, text='Register', command=lambda: self.update_player_list())
-        addToAll.grid(column=0, row=0, sticky=(N,W,E,S))
+        addToAll = Button(repositoriFrame, text='Register', command=lambda: self.register_user())
+        addToAll.grid(column=0, row=0, sticky=(N, W, E, S))
         # list of all users
         self.listAll = Listbox(repositoriFrame, height=10, width=52)
         scrollAll = Scrollbar(repositoriFrame, orient=VERTICAL, command=self.listAll.yview)
-        scrollAll.grid(column=1, row=1, sticky=(N,S))
+        scrollAll.grid(column=1, row=1, sticky=(N, S))
         self.listAll['yscrollcommand'] = scrollAll.set
         self.listAll.grid(column=0, row=1)
         self.update_player_list()
 
         # up mid
-        self.mid_bar = Frame(self.content, width= 128, height=128)
+        self.mid_bar = Frame(self.content, width=128, height=128)
         self.mid_bar.grid(column=1, row=0, padx=16, pady=16)
         # add user to game
         self.addToGame = Button(self.mid_bar, text='Add >>', command=lambda: self.addUser())
-        self.addToGame.grid(column=0, row=0, pady=8, sticky=(W,E))
+        self.addToGame.grid(column=0, row=0, pady=8, sticky=(W, E))
         # remove user from game
         self.removeFromGame = Button(self.mid_bar, text='<< Remove', command=lambda: self.removeUser())
-        self.removeFromGame.grid(column=0, row=1, pady=8, sticky=(W,E))
-        self.profile = Button(self.mid_bar, text='Profile', command=lambda: self.show_profile(Profile))
-        self.profile.grid(column=0, row=2, pady=8, sticky=(W,E))
+        self.removeFromGame.grid(column=0, row=1, pady=8, sticky=(W, E))
+        self.profile = Button(self.mid_bar, text='Profile', command=lambda: self.show_profile())
+        self.profile.grid(column=0, row=2, pady=8, sticky=(W, E))
         self.generateGame = Button(self.mid_bar, text='Generate', command=lambda: self.generate_game())
-        self.generateGame.grid(column=0, row=3, pady=8, sticky=(W,E))
-
+        self.generateGame.grid(column=0, row=3, pady=8, sticky=(W, E))
 
         # up right
         self.playerNotebook = ttk.Notebook(self.content)
         self.playerNotebook.grid(column=2, row=0, padx=16, pady=16)
-        self.playerFrame = Frame(self.playerNotebook, width= 128, height=128)
+        self.playerFrame = Frame(self.playerNotebook, width=128, height=128)
         self.playerFrame.grid(column=0, row=0, padx=16, pady=16)
-        self.lobbyFrame = Frame(self.playerNotebook,width= 128, height=128)
+        self.lobbyFrame = Frame(self.playerNotebook, width=128, height=128)
         self.lobbyFrame.grid(column=0, row=0)
 
         self.txt = Text(self.lobbyFrame, height=10, width=39)
@@ -74,7 +74,7 @@ class MainWindow:
 
         # down left
         # this should have all the data for team 1
-        self.teamBlueFrame = Frame(self.content, width= 128, height=128)
+        self.teamBlueFrame = Frame(self.content, width=128, height=128)
         self.teamBlueFrame.grid(column=0, row=1)
         self.teamBlueLabel = Label(self.teamBlueFrame, text="Blue team")
         self.teamBlueLabel.grid(column=0, row=0)
@@ -98,15 +98,14 @@ class MainWindow:
 
         # down right
         # this should have all the data for team 2
-        self.teamRedFrame = Frame(self.content, width= 128, height=128)
+        self.teamRedFrame = Frame(self.content, width=128, height=128)
         self.teamRedFrame.grid(column=2, row=1, padx=16, pady=16)
         self.teamRedLabel = Label(self.teamRedFrame, text="Red team")
         self.teamRedLabel.grid(column=0, row=0)
         self.listRed = Listbox(self.teamRedFrame, height=5, width=52)
         self.listRed.grid(column=0, row=1)
 
-
-    def show_profile(self, _class):
+    def show_profile(self):
         try:
             if self.new.state() == "normal":
                 self.new.focus()
@@ -118,22 +117,32 @@ class MainWindow:
                 name = None
             if name:
                 self.new = Toplevel(self.root)
-                _class(self.new, name, self.c)
+                self.new.title(name + "'s Profile")
+                Profile(self.new, name, self.c)
+
+    def register_user(self):
+        try:
+            if self.new.state() == "normal":
+                self.new.focus()
+        except:
+            self.new = Toplevel(self.root)
+            self.new.title("Register User")
+            Register(self.new, self.c)
 
     def removeUser(self):
-        if(0 < len(self.listPlayers.curselection())):
+        if 0 < len(self.listPlayers.curselection()):
             UsersGame.remove(self.listPlayers.get(self.listPlayers.curselection()[0]))
             self.listPlayers.delete(self.listPlayers.curselection()[0])
 
     def addUser(self):
-        if(0 < len(self.listAll.curselection()) and self.listPlayers.size() < MaxPlayers
-        and not self.listAll.get(self.listAll.curselection()[0]) in UsersGame):
+        if (0 < len(self.listAll.curselection()) and self.listPlayers.size() < MaxPlayers
+                and not self.listAll.get(self.listAll.curselection()[0]) in UsersGame):
             user = self.listAll.get(self.listAll.curselection()[0])
             UsersGame.add(user)
             self.listPlayers.insert('end', user)
-            if(self.listAll.curselection()[0]+1 < self.listAll.size()):
-                select = self.listAll.curselection()[0]+1
-                self.listAll.select_clear(select-1)
+            if self.listAll.curselection()[0] + 1 < self.listAll.size():
+                select = self.listAll.curselection()[0] + 1
+                self.listAll.select_clear(select - 1)
                 self.listAll.selection_set(select)
 
     def update_player_list(self):
@@ -141,18 +150,20 @@ class MainWindow:
         playersAll = list()
         for i in self.c.player_list.players:
             playersAll.append(i)
-        playersAll = sorted(playersAll, key= lambda x:x.lower())
-        j=0
+        playersAll = sorted(playersAll, key=lambda x: x.lower())
+        j = 0
         for i in playersAll:
             self.listAll.insert('end', i)
-            if j%2: self.listAll.itemconfigure(j, background='#AEB7B3')
-            else: self.listAll.itemconfigure(j, background='#E1EFE6')
-            j+=1
+            if j % 2:
+                self.listAll.itemconfigure(j, background='#AEB7B3')
+            else:
+                self.listAll.itemconfigure(j, background='#E1EFE6')
+            j += 1
 
     def generate_game(self):
-        if(self.playerNotebook.index("current") < 1):
-            #print("generating game from list")
-            if (9 < self.listPlayers.size()):
+        if self.playerNotebook.index("current") < 1:
+            # print("generating game from list")
+            if 9 < self.listPlayers.size():
                 # self.c.
                 self.listBlue.delete(0, END)
                 self.listRed.delete(0, END)
@@ -174,7 +185,7 @@ class MainWindow:
                 self.listBlue.update()
                 self.listRed.update()
         else:
-            #print("generating game from lobby")
+            # print("generating game from lobby")
             self.listBlue.delete(0, END)
             self.listRed.delete(0, END)
             f = open("../Controller/text_test", "w", encoding="utf8")
@@ -193,10 +204,24 @@ class MainWindow:
             self.listRed.update()
             self.update_player_list()
 
-class GetFromLobby:
-    def __init__(self, root):
+
+class Register:
+    def __init__(self, root, ctr):
+
         self.root = root
 
+        self.outer_frame = Frame(self.root)
+        self.name_frame = Frame(self.outer_frame,height=50, width=420)
+        self.profile_frame = Frame(self.outer_frame, height=500, width=420)
+
+        self.name_label = Label(self.outer_frame, text="Summoner Name:", font="Helvetica 14")
+        self.name_entry = Entry(self.outer_frame, font = "Helvetica 14 bold")
+
+        self.name_label.place(x=10,y=10)
+        self.name_entry.place(x=180,y=12)
+
+        self.outer_frame.grid(column=0, row=0)
+        self.name_frame.grid(column=0, row=0)
 
 
 class Profile:
@@ -210,7 +235,6 @@ class Profile:
         self.role_frame = Frame(self.right_frame)
         self.ctr = ctr
         self.name = name
-
         # Labels, Combobox, etc.
 
         # El Exum
